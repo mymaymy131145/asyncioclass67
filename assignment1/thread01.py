f301bb86472b79b1ca04ae0e5f2f0201740b022a
mymaymy131145 <108925582+mymaymy131145@users.noreply.github.com>
2024-06-21 09:57:39 +0700

    1

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -1,1 +1,14 @@
 # running a function in another thread
+from time import sleep
+from threading import Thread
+
+def task():
+    while True:
+        print("This is from another thread")
+        sleep(2)
+
+thread = Thread(target=task)
+thread.start()
+
+print("Waiting for the thread...")
+thread.join()
